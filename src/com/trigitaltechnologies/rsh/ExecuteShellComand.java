492b413ac1ee6a778e0af274902916a68a9ebfe7
manojanandilla <manojanand432@gmail.com>
2015-09-28 22:00:29 +0530

    Initial Commit

--- src/com/trigitaltechnologies/rsh/ExecuteShellComand.java
+++ src/com/trigitaltechnologies/rsh/ExecuteShellComand.java
@@ -4,40 +4,54 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.util.Properties;
+import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class ExecuteShellComand {
 
 	public static void main(String[] args) {
+		final ExecuteShellComand esc = new ExecuteShellComand();
+		final ResourceBundle resourceBundle = ResourceBundle.getBundle("config");
 
-		ExecuteShellComand esc = new ExecuteShellComand();
-		String command = "rsh -l root 125.99.127.30 shcm";
-		String output = esc.executeCommand(command);
-		System.out.println("Succesfully executed the command");
+		// Task to Run Goes Here
+		Runnable runnable = new Runnable() {
+			public void run() {
+				System.out.println("Process started");
+				for (String cmtsIP : resourceBundle.getString("CMTS_IP").split(",")) {
+					esc.executeCommand("rsh -l root "+cmtsIP+" shcm", cmtsIP);
+					System.out.println("Process started");
+				}
+			}
+		};
+
+		ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+		service.scheduleAtFixedRate(runnable, 0, 10000, TimeUnit.MILLISECONDS);
+		
 	}
 
-	private String executeCommand(String command) {
+	private String executeCommand(String command, String cmtsIP) {
 
 		StringBuffer output = new StringBuffer();
 		File file;
 		Process p;
 		try {
 			p = Runtime.getRuntime().exec(command);
-			//System.out.println(p);
-			
-			BufferedReader reader = 
-                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					p.getInputStream()));
 
-                        String line = "";	
-            file = new File("cm.txt");
-            FileWriter fileWriter = new FileWriter(file);
-			while ((line = reader.readLine())!= null) {
+			String line = "";
+			file = new File("shcm_" + cmtsIP + ".txt");
+			FileWriter fileWriter = new FileWriter(file);
+			while ((line = reader.readLine()) != null) {
 				output.append(line + "\n");
-				fileWriter.write(line+System.lineSeparator());
-				
-				System.out.println("+"+line);
+				fileWriter.write(line + System.lineSeparator());
+				System.out.println("+" + line);
 			}
 			fileWriter.flush();
-			fileWriter.close();	
+			fileWriter.close();
 			p.destroy();
 
 		} catch (Exception e) {
